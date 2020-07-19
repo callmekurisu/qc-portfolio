fa09d9372783fd26bb75f911166d8beb6b11e600
callmekurisu <callmekurisu@gmail.com>
2020-07-19 18:26:30 -0400

    update binet

--- binet.py
+++ binet.py
@@ -2,8 +2,8 @@ import math
 import sys
 
 # pass fn as argument to script for fibonacci value
-input = int(sys.argv[1])
+ui = int(input('Enter fn: '))
 phi = 1.618
 psi = 1-phi
-fn = math.ceil((phi**input-psi**input) / (phi - psi))
+fn = math.ceil((phi**ui-psi**ui) / (phi - psi))
 print(fn)
